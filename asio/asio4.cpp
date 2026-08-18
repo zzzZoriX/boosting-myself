a3a186bcb5045757861aa437f3faeec972b705e9
zzzZoriX <zorix228337@gmail.com>
2026-08-18 13:04:10 +0300

    create new .cpp file for day 4

---
+++ asio/asio4.cpp
@@ -0,0 +1,15 @@
+#include <deque>
+#include <set>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <memory>
+#include <array>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <iostream>
+#include <string>
+
+using namespace boost;
+namespace ssl = asio::ssl;
+using asio::ip::tcp;
