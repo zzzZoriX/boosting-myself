d3f4ea5480a5fd6c615cfcb4285389b9712554ff
zzzZoriX <zorix228337@gmail.com>
2026-08-18 14:07:18 +0300

    write simple tsl server

--- asio/asio3.cpp
+++ asio/asio3.cpp
@@ -193,235 +193,235 @@ using asio::ip::udp;
 // }
 
 // 3.2 -- выполнено
-class chatroom;
+// class chatroom;
 
-class session: public std::enable_shared_from_this<session> {
-    std::deque<std::string> write_queue;
+// class session: public std::enable_shared_from_this<session> {
+//     std::deque<std::string> write_queue;
 
-    tcp::socket sock;
-    std::array<char, 4096> buffer;
-    asio::strand<asio::any_io_executor> strand;
+//     tcp::socket sock;
+//     std::array<char, 4096> buffer;
+//     asio::strand<asio::any_io_executor> strand;
 
-    std::weak_ptr<chatroom> chat;
+//     std::weak_ptr<chatroom> chat;
 
 
-    void close_connection();
-    void do_write();
-    void read();
+//     void close_connection();
+//     void do_write();
+//     void read();
 
-public:
-    bool stopping;
+// public:
+//     bool stopping;
 
-    session(tcp::socket&& sock, std::shared_ptr<chatroom> chat):
-        sock(std::move(sock)), strand(sock.get_executor()), chat(chat)
-        {}
+//     session(tcp::socket&& sock, std::shared_ptr<chatroom> chat):
+//         sock(std::move(sock)), strand(sock.get_executor()), chat(chat)
+//         {}
 
-    ~session() {
-        system::error_code ec;
+//     ~session() {
+//         system::error_code ec;
 
-        sock.close(ec);
-    }
+//         sock.close(ec);
+//     }
 
-    void start() {
-        read();
-    }
+//     void start() {
+//         read();
+//     }
 
-    void send(std::string message) {
-        asio::post(strand, [this, self = shared_from_this(), msg = std::move(message)](){
-            bool wip{!write_queue.empty()};
-            write_queue.push_back(std::move(msg));
+//     void send(std::string message) {
+//         asio::post(strand, [this, self = shared_from_this(), msg = std::move(message)](){
+//             bool wip{!write_queue.empty()};
+//             write_queue.push_back(std::move(msg));
 
-            if(!wip) {
-                do_write();               
-            }
-        });
-    }
+//             if(!wip) {
+//                 do_write();               
+//             }
+//         });
+//     }
 
-    tcp::endpoint get_ep() {
-        return sock.local_endpoint();
-    }
-};
+//     tcp::endpoint get_ep() {
+//         return sock.local_endpoint();
+//     }
+// };
 
-class chatroom {
-    std::set<std::shared_ptr<session>> sessions;
-    asio::strand<asio::any_io_executor> strand;
+// class chatroom {
+//     std::set<std::shared_ptr<session>> sessions;
+//     asio::strand<asio::any_io_executor> strand;
     
-public:
-    chatroom(asio::io_context& ioc):
-        strand(asio::make_strand(ioc))
-        {}
+// public:
+//     chatroom(asio::io_context& ioc):
+//         strand(asio::make_strand(ioc))
+//         {}
 
-    void join(std::shared_ptr<session> session){
-        asio::post(strand, [this, session]{
-            sessions.emplace(session);
-        });
-    }
+//     void join(std::shared_ptr<session> session){
+//         asio::post(strand, [this, session]{
+//             sessions.emplace(session);
+//         });
+//     }
 
-    void leave(std::shared_ptr<session> session) {
-        asio::post(strand, [this, session]{
-            sessions.erase(session);
-        });
-    }
+//     void leave(std::shared_ptr<session> session) {
+//         asio::post(strand, [this, session]{
+//             sessions.erase(session);
+//         });
+//     }
 
-    void leave_all() {
-        asio::post(strand, [this]{
-            for(auto& s: sessions)
-                s->stopping = true;
-        });
-    }
+//     void leave_all() {
+//         asio::post(strand, [this]{
+//             for(auto& s: sessions)
+//                 s->stopping = true;
+//         });
+//     }
 
-    void deliver(const std::string& sender_msg, std::shared_ptr<session> sender) {
-        asio::post(strand, [this, sender_msg, sender]{
-            std::stringstream msgs;
+//     void deliver(const std::string& sender_msg, std::shared_ptr<session> sender) {
+//         asio::post(strand, [this, sender_msg, sender]{
+//             std::stringstream msgs;
 
-            msgs << sender->get_ep() << ": " << sender_msg << std::endl;
+//             msgs << sender->get_ep() << ": " << sender_msg << std::endl;
 
-            std::string msg{msgs.str()};
+//             std::string msg{msgs.str()};
 
-            for(const auto& s: sessions) {
-                if(s != sender) {
-                    s->send(msg);
-                }
-            }
-        });
-    }
+//             for(const auto& s: sessions) {
+//                 if(s != sender) {
+//                     s->send(msg);
+//                 }
+//             }
+//         });
+//     }
 
-    void server_deliver(const std::string& msg) {
-        asio::post(strand, [this, msg]{
-            for(const auto& s: sessions)
-                s->send(msg);
-        });
-    }
-};
+//     void server_deliver(const std::string& msg) {
+//         asio::post(strand, [this, msg]{
+//             for(const auto& s: sessions)
+//                 s->send(msg);
+//         });
+//     }
+// };
 
-void session::close_connection() {
-    system::error_code ec;
+// void session::close_connection() {
+//     system::error_code ec;
 
-    sock.close(ec);
+//     sock.close(ec);
 
-    if (ec) std::cout << ec.message() << std::endl;
+//     if (ec) std::cout << ec.message() << std::endl;
 
-    if(auto chat_ptr = chat.lock()) {
-        chat_ptr->leave(shared_from_this());
-    }
-}
+//     if(auto chat_ptr = chat.lock()) {
+//         chat_ptr->leave(shared_from_this());
+//     }
+// }
 
-void session::do_write() {
-    asio::async_write(
-        sock,
-        asio::buffer(write_queue.front()),
-        asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t) {
-            if(!ec) {
-                write_queue.pop_front();
+// void session::do_write() {
+//     asio::async_write(
+//         sock,
+//         asio::buffer(write_queue.front()),
+//         asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t) {
+//             if(!ec) {
+//                 write_queue.pop_front();
 
-                if(!write_queue.empty()){
-                    do_write();
-                }
-                else if(stopping){
-                    close_connection();
-                }
-            }
-            else {
-                std::cout << ec.message() << std::endl;
+//                 if(!write_queue.empty()){
+//                     do_write();
+//                 }
+//                 else if(stopping){
+//                     close_connection();
+//                 }
+//             }
+//             else {
+//                 std::cout << ec.message() << std::endl;
 
-                write_queue.clear();
-            } 
-        })
-    );
-}
+//                 write_queue.clear();
+//             } 
+//         })
+//     );
+// }
 
-void session::read() {
-    sock.async_read_some(
-        asio::buffer(buffer),
-        [this, self = shared_from_this()](system::error_code ec, std::size_t len){
-            if (!ec){
-                std::string msg{buffer.data()};
+// void session::read() {
+//     sock.async_read_some(
+//         asio::buffer(buffer),
+//         [this, self = shared_from_this()](system::error_code ec, std::size_t len){
+//             if (!ec){
+//                 std::string msg{buffer.data()};
 
-                if(auto chat_ptr = chat.lock()) {
-                    chat_ptr->deliver(msg, shared_from_this());
-                }
+//                 if(auto chat_ptr = chat.lock()) {
+//                     chat_ptr->deliver(msg, shared_from_this());
+//                 }
 
-                read();
-            }
-            else{
-                std::cout << ec.message() << std::endl;
+//                 read();
+//             }
+//             else{
+//                 std::cout << ec.message() << std::endl;
 
-                close_connection();
-            }
-        }
-    );
-}
+//                 close_connection();
+//             }
+//         }
+//     );
+// }
 
-class server: public std::enable_shared_from_this<server> {
-    std::shared_ptr<chatroom> chat;
+// class server: public std::enable_shared_from_this<server> {
+//     std::shared_ptr<chatroom> chat;
     
-    tcp::acceptor acceptor;
-    int port;
+//     tcp::acceptor acceptor;
+//     int port;
 
-    asio::io_context& ioc;
-    asio::signal_set sig_set;
-
-
-    void do_accept() {
-        acceptor.async_accept([this](system::error_code ec, tcp::socket sock){
-            if(!ec) {
-                std::cout << "New connection: " << sock.local_endpoint() << std::endl;
-
-                auto session_ptr = std::make_shared<session>(std::move(sock), chat);
-                session_ptr->stopping = false;
-
-                chat->join(session_ptr);
-
-                session_ptr->start();
-
-                do_accept();
-            }
-            else{
-                std::cout << ec.message() << std::endl;
-            }
-        });
-    }
-
-public:
-    server(asio::io_context& ioc, const int port):  
-        port(port), acceptor(ioc, tcp::endpoint(tcp::v4(), port)), chat(std::make_shared<chatroom>(ioc)), ioc(ioc), sig_set(ioc, SIGINT, SIGTERM)
-        {}
-
-    void start() {
-        std::cout << "Server running. Press ctrl+c to stop." << std::endl;
+//     asio::io_context& ioc;
+//     asio::signal_set sig_set;
 
 
-        sig_set.async_wait([this, self = shared_from_this()](system::error_code ec, int){
-            if(!ec) {
-                std::cout << "\nServer marked as stoped." << std::endl;
+//     void do_accept() {
+//         acceptor.async_accept([this](system::error_code ec, tcp::socket sock){
+//             if(!ec) {
+//                 std::cout << "New connection: " << sock.local_endpoint() << std::endl;
 
-                acceptor.close();
+//                 auto session_ptr = std::make_shared<session>(std::move(sock), chat);
+//                 session_ptr->stopping = false;
 
-                chat->server_deliver("Server stoped.");
-                chat->leave_all();
-            }
-        });
+//                 chat->join(session_ptr);
 
-        do_accept();
-    }
-};
+//                 session_ptr->start();
+
+//                 do_accept();
+//             }
+//             else{
+//                 std::cout << ec.message() << std::endl;
+//             }
+//         });
+//     }
+
+// public:
+//     server(asio::io_context& ioc, const int port):  
+//         port(port), acceptor(ioc, tcp::endpoint(tcp::v4(), port)), chat(std::make_shared<chatroom>(ioc)), ioc(ioc), sig_set(ioc, SIGINT, SIGTERM)
+//         {}
+
+//     void start() {
+//         std::cout << "Server running. Press ctrl+c to stop." << std::endl;
 
 
-int main() {
-    try {
-        asio::io_context ioc;
+//         sig_set.async_wait([this, self = shared_from_this()](system::error_code ec, int){
+//             if(!ec) {
+//                 std::cout << "\nServer marked as stoped." << std::endl;
 
-        auto serv{std::make_shared<server>(ioc, 1234)};
+//                 acceptor.close();
 
-        serv->start();
+//                 chat->server_deliver("Server stoped.");
+//                 chat->leave_all();
+//             }
+//         });
 
-        ioc.run();
-    }
-    catch(const std::exception& e) {
-        std::cout << e.what() << std::endl;
+//         do_accept();
+//     }
+// };
+
+
+// int main() {
+//     try {
+//         asio::io_context ioc;
+
+//         auto serv{std::make_shared<server>(ioc, 1234)};
+
+//         serv->start();
+
+//         ioc.run();
+//     }
+//     catch(const std::exception& e) {
+//         std::cout << e.what() << std::endl;
     
-        return 1;
-    }
+//         return 1;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
--- asio/asio4.cpp
+++ asio/asio4.cpp
@@ -13,3 +13,126 @@
 using namespace boost;
 namespace ssl = asio::ssl;
 using asio::ip::tcp;
+
+
+// class tsl_session: public std::enable_shared_from_this<tsl_session> {
+//     ssl::stream<tcp::socket> stream;
+//     std::array<char, 4096> buffer;
+
+//     asio::signal_set sig_set;
+
+
+//     void close_connection() {
+//         stream.async_shutdown([this, self = shared_from_this()](system::error_code ec){
+//             if(!ec) {
+//                 std::cout << "disconnect" << std::endl;
+
+//                 system::error_code close_ec;
+//                 stream.lowest_layer().close(close_ec);
+//             }
+//         });
+//     }
+
+//     void read() {
+//         stream.async_read_some(
+//             asio::buffer(buffer),
+//             [this, self = shared_from_this()](system::error_code ec, std::size_t bytes) {
+//                 if(!ec) {
+//                     write(bytes);
+//                 }
+//             }
+//         );
+//     }
+
+//     void write(std::size_t len) {
+//         asio::async_write(
+//             stream, 
+//             asio::buffer(buffer, len),
+//             [this, self = shared_from_this()](system::error_code ec, std::size_t) {
+//                 if(!ec) {
+//                     read();
+//                 }
+//             }
+//         );
+//     }
+
+// public:
+//     tsl_session(asio::io_context& ioc, tcp::socket&& sock, ssl::context& ctx):
+//         stream(std::move(sock), ctx), sig_set(ioc, SIGINT, SIGTERM) {}
+
+//     void start() {
+//         sig_set.async_wait([this, self = shared_from_this()](system::error_code ec, int){
+//             if(!ec) {
+//                 close_connection();
+//             }
+//         });
+
+//         stream.async_handshake(
+//             ssl::stream_base::server, 
+//             [this, self = shared_from_this()](system::error_code ec) {
+//                 if(!ec) {
+//                     read();                   
+//                 }
+//                 else {
+//                     sig_set.cancel();
+//                     close_connection();
+//                 }
+//             }
+//         );
+//     }
+// };
+
+// class tsl_server {
+//     tcp::acceptor acceptor;
+//     asio::io_context& ioc;
+//     ssl::context& ctx;
+
+//     void do_accept() {
+//         acceptor.async_accept([this](system::error_code ec, tcp::socket sock){
+//             if(!ec) {
+//                 std::make_shared<tsl_session>(ioc, std::move(sock), ctx)->start();
+
+//                 std::cout << "new connection" << std::endl;
+//             }
+//         }); 
+//     }
+
+// public:
+//     tsl_server(asio::io_context& ioc, ssl::context& ctx, const int port):
+//         acceptor(ioc, tcp::endpoint(tcp::v4(), port)), ctx(ctx), ioc(ioc) 
+//     {
+//         do_accept();
+//     }
+// };
+
+
+// int main() {
+//     try {
+//         asio::io_context ioc;
+
+//         ssl::context ctx{ssl::context::tlsv12_server};
+
+//         ctx.set_options(
+//             ssl::context::default_workarounds |
+//             ssl::context::no_sslv2 |
+//             ssl::context::no_sslv3 |
+//             ssl::context::single_dh_use
+//         );
+
+//         ctx.use_certificate_chain_file("server.crt");
+//         ctx.use_private_key_file("server.key", ssl::context::pem);
+
+//         tsl_server serv{ioc, ctx, 1234};
+        
+//         ioc.run();
+//     }
+//     catch(const std::exception& e) {
+//         std::cout << e.what() << std::endl;
+
+//         return 1;
+//     }
+
+//     return 0;
+// }
+
+
