fd259edd29bad8148fdf116b01d409577efb5d77
zzzZoriX <zorix228337@gmail.com>
2026-08-18 22:06:25 +0300

    practice task 4.2

--- asio/asio4.cpp
+++ asio/asio4.cpp
@@ -138,133 +138,268 @@ using asio::ip::tcp;
 
 // задания
 // 4.1: SSL Echo-Сервер -- выполнено
-class tsl_session: public std::enable_shared_from_this<tsl_session> {
+// class tsl_session: public std::enable_shared_from_this<tsl_session> {
+//     asio::streambuf buffer;
+//     ssl::stream<tcp::socket> stream;
+
+//     asio::strand<asio::any_io_executor> strand;
+
+
+//     void shutdown() {
+//         stream.async_shutdown(asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec){
+//             if(!ec) {
+//                 std::cout << "Client " << stream.lowest_layer().remote_endpoint() << " disconnected" << std::endl;
+                
+//                 system::error_code close_ec;
+//                 stream.lowest_layer().close(close_ec);
+//             }
+//         }));
+//     }
+
+//     void read() {
+//         asio::async_read_until(
+//             stream,
+//             buffer,
+//             "\n",
+//             asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t len) {
+//                 if(!ec) {
+//                     std::cout << "new message received" << std::endl;
+
+//                     write(len);
+//                 }
+//                 else {
+//                     std::cout << ec.message() << std::endl;
+
+//                     shutdown();
+//                 }
+//             })
+//         );
+//     }
+
+//     void write(std::size_t bytes) {
+//         asio::async_write(
+//             stream,
+//             buffer,
+//             asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t){
+//                 if(!ec) {
+//                     read();
+//                 }
+//                 else {
+//                     std::cout << ec.message() << std::endl;
+
+//                     shutdown();
+//                 }
+//             })
+//         );
+//     }
+
+// public:
+//     tsl_session(tcp::socket&& sock, ssl::context& ctx, asio::io_context& ioc):
+//         stream(std::move(sock), ctx), strand(asio::make_strand(ioc)) {}
+        
+//     void start() {
+//         stream.async_handshake(
+//             ssl::stream_base::server,
+//             [this, self = shared_from_this()](system::error_code ec){
+//                 if(!ec) {
+//                     read();
+//                 }
+//                 else {
+//                     shutdown();
+//                 }
+//             }
+//         );
+//     }
+// };
+
+// class tsl_server {
+//     asio::io_context& ioc;
+//     tcp::acceptor acceptor;
+//     ssl::context& ctx;
+
+//     const int port;
+
+
+//     void do_accept() {
+//         acceptor.async_accept([this](system::error_code ec, tcp::socket sock){
+//             if(!ec) {
+//                 std::cout << "new connection from " << sock.remote_endpoint() << std::endl;
+
+//                 std::make_shared<tsl_session>(std::move(sock), ctx, ioc)->start();
+//             }
+//             else {
+//                 std::cout << ec.message() << std::endl;
+//             }
+
+//             do_accept();
+//         });
+//     }
+
+// public:
+//     tsl_server(asio::io_context& ioc, const int port, ssl::context& ctx):
+//         port(port), ioc(ioc), ctx(ctx), acceptor(ioc, tcp::endpoint(tcp::v4(), port)) {}
+
+//     void start() {
+//         std::cout << "server running on 127.0.0.1:" << port << std::endl;
+
+//         do_accept();
+//     }
+// };
+
+
+// int main() {
+//     try {
+//         asio::io_context ioc;
+
+//         ssl::context ctx{ssl::context::tlsv12_server};
+
+//         ctx.set_options(
+//             ssl::context::default_workarounds |
+//             ssl::context::no_sslv2 |
+//             ssl::context::no_sslv3 |
+//             ssl::context::single_dh_use
+//         );
+
+//         ctx.use_certificate_chain_file("server.crt");
+//         ctx.use_private_key_file("server.key", ssl::context::pem);
+
+//         tsl_server serv{ioc, 1234, ctx};
+//         serv.start();
+
+//         ioc.run();
+//     }
+//     catch(const std::exception& e) {
+//         std::cerr << e.what() << std::endl;
+
+//         return 1;
+//     }
+
+//     return 0;
+// }
+
+// 4.2 Парсер команд -- выполняется
+class commands_processor;
+
+class command: public std::enable_shared_from_this<command> {
+    tcp::socket sock;
     asio::streambuf buffer;
-    ssl::stream<tcp::socket> stream;
 
     asio::strand<asio::any_io_executor> strand;
 
+    std::weak_ptr<commands_processor> cmd_proc;
 
-    void shutdown() {
-        stream.async_shutdown(asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec){
-            if(!ec) {
-                std::cout << "Client " << stream.lowest_layer().remote_endpoint() << " disconnected" << std::endl;
-                
-                system::error_code close_ec;
-                stream.lowest_layer().close(close_ec);
-            }
-        }));
-    }
 
-    void read() {
-        asio::async_read_until(
-            stream,
-            buffer,
-            "\n",
-            asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t len) {
-                if(!ec) {
-                    std::cout << "new message received" << std::endl;
-
-                    write(len);
-                }
-                else {
-                    std::cout << ec.message() << std::endl;
-
-                    shutdown();
-                }
-            })
-        );
-    }
-
-    void write(std::size_t bytes) {
-        asio::async_write(
-            stream,
-            buffer,
-            asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t){
-                if(!ec) {
-                    read();
-                }
-                else {
-                    std::cout << ec.message() << std::endl;
-
-                    shutdown();
-                }
-            })
-        );
-    }
+    void read();
 
 public:
-    tsl_session(tcp::socket&& sock, ssl::context& ctx, asio::io_context& ioc):
-        stream(std::move(sock), ctx), strand(asio::make_strand(ioc)) {}
-        
+    command(asio::io_context& ioc, tcp::socket&& sock, std::shared_ptr<commands_processor> cmd_proc): 
+        sock(std::move(sock)), strand(asio::make_strand(ioc)), cmd_proc(cmd_proc) {}
+
     void start() {
-        stream.async_handshake(
-            ssl::stream_base::server,
-            [this, self = shared_from_this()](system::error_code ec){
-                if(!ec) {
-                    read();
+        read();
+    }
+
+    void send(std::string msg) {
+        asio::async_write(
+            sock,
+            asio::buffer(msg),
+            asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t){
+                if(ec) {
+                    std::cout << ec.message() << std::endl;
                 }
-                else {
-                    shutdown();
-                }
-            }
+            })
         );
     }
 };
 
-class tsl_server {
-    asio::io_context& ioc;
+class commands_processor: public std::enable_shared_from_this<commands_processor> {
     tcp::acceptor acceptor;
-    ssl::context& ctx;
-
     const int port;
+
+    asio::strand<asio::any_io_executor> strand;
+
+    asio::io_context& ioc;
 
 
     void do_accept() {
-        acceptor.async_accept([this](system::error_code ec, tcp::socket sock){
+        acceptor.async_accept(asio::bind_executor(strand, [this](system::error_code ec, tcp::socket sock){
             if(!ec) {
-                std::cout << "new connection from " << sock.remote_endpoint() << std::endl;
+                std::cout << "new connection" << std::endl;
 
-                std::make_shared<tsl_session>(std::move(sock), ctx, ioc)->start();
+                std::make_shared<command>(ioc, std::move(sock), shared_from_this())->start();
+                
+                do_accept();
             }
             else {
                 std::cout << ec.message() << std::endl;
             }
-
-            do_accept();
-        });
+        }));
     }
 
 public:
-    tsl_server(asio::io_context& ioc, const int port, ssl::context& ctx):
-        port(port), ioc(ioc), ctx(ctx), acceptor(ioc, tcp::endpoint(tcp::v4(), port)) {}
+    commands_processor(asio::io_context& ioc, const int port):
+        strand(asio::make_strand(ioc)), port(port), acceptor(ioc, tcp::endpoint(tcp::v4(), port)), ioc(ioc) {}
 
     void start() {
-        std::cout << "server running on 127.0.0.1:" << port << std::endl;
-
         do_accept();
     }
+
+    void process_command(std::shared_ptr<command> cmd_ptr, std::string command) {
+        std::cout << "command received: " << std::move(command);
+
+        while (!command.empty() && 
+          (command.back() == '\r' || 
+           command.back() == '\n' || 
+           command.back() == ' '  || 
+           command.back() == '\t')) 
+        {
+            command.pop_back();
+        }
+
+        if(command == "QUIT" || command == "QUIT\r") {
+            asio::post(strand, [this, self = shared_from_this()]{ 
+                system::error_code close_ec;
+                acceptor.close(close_ec);
+
+                ioc.stop();
+            });
+        }
+        else if(command == "PING" || command == "PING\r") {
+            asio::post(strand, [this, cmd_ptr, self = shared_from_this()]{
+                cmd_ptr->send("PONG");
+            });
+        }
+    }
 };
+
+void command::read() {
+    asio::async_read_until(
+        sock,
+        buffer,
+        "\n",
+        asio::bind_executor(strand, [this, self = shared_from_this()](system::error_code ec, std::size_t len) {
+            if(!ec) {
+                if(auto cmd_proc_ptr = cmd_proc.lock())
+                    cmd_proc_ptr->process_command(shared_from_this(), std::string(asio::buffers_begin(buffer.data()), asio::buffers_begin(buffer.data()) + len));
+
+                buffer.consume(len);
+
+                read();
+            }
+            else {
+                std::cout << ec.message() << std::endl;
+            }
+        })
+    );
+}
 
 
 int main() {
     try {
         asio::io_context ioc;
 
-        ssl::context ctx{ssl::context::tlsv12_server};
-
-        ctx.set_options(
-            ssl::context::default_workarounds |
-            ssl::context::no_sslv2 |
-            ssl::context::no_sslv3 |
-            ssl::context::single_dh_use
-        );
-
-        ctx.use_certificate_chain_file("server.crt");
-        ctx.use_private_key_file("server.key", ssl::context::pem);
-
-        tsl_server serv{ioc, 1234, ctx};
-        serv.start();
+        auto proc = std::make_shared<commands_processor>(ioc, 1234);
+        proc->start();
 
         ioc.run();
     }
